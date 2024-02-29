96d69799703ad9833a01b510f439bcfc19a04f98
siarune <aidan.sharp@siarune.dev>
2024-02-29 14:38:40 -0500

    bump to solid.js 0.6

--- app.config.ts
+++ app.config.ts
@@ -2,8 +2,8 @@ import { defineConfig } from "@solidjs/start/config";
 import UnoCSS from "unocss/vite";
 
 export default defineConfig({
-  start: {
-    ssr: true
-  },
-  plugins: [UnoCSS()],
+  ssr: true,
+  vite: {
+    plugins: [UnoCSS()]
+  }
 });
